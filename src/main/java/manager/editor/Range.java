11935fdf9ec495734569c96c86102bee105b8590
Patryk Mikos <patryk912@gmail.com>
2012-04-06 01:35:55 +0200

    Proba commitu z Baazara  zmiana i implementacja klasy Range

--- src/main/java/manager/editor/Range.java
+++ src/main/java/manager/editor/Range.java
@@ -1,30 +1,38 @@
 package manager.editor;
 
 public class Range {
-    public Range(float min, float max) {
+	private float mMin, mMax, mValue;
+    public Range(float a, float b) {
+    	mMin = Math.min(a, b);
+    	mMax = Math.max(a, b);
     }
 
     public float getValue() {
-        return 0;
+        return mValue;
     }
 
     public float getMin() {
-        return 0;
+        return mMin;
     }
 
     public float getMax() {
-        return 0;
+        return mMax;
+    }
+    
+    public void interpolate(float value){
+    	if(value > 1.0f) value = 1.0f;
+    	if(value < 0.0f) value = 0.0f;
+    	mValue = mMin + (mMax-mMin)*value;
     }
 
     public void setValue(float value) {
-    }
-
-    public boolean isOK() {
-        return false;
+    	if(value < mMin) mValue = mMin;
+    	else if(value > mMax) mValue = mMax;
+    	else mValue = value;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return mMin + ";" + mMax;
     }
 }
