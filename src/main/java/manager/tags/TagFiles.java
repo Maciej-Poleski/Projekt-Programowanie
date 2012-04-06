e1fcb2a116eb4c6f566b24c2532814286e2926ee
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2012-04-06 19:15:17 +0200

    Revert zmian z r11

--- src/main/java/manager/tags/TagFiles.java
+++ src/main/java/manager/tags/TagFiles.java
@@ -1,66 +1,62 @@
 package manager.tags;
 
-import manager.files.FileID;
+import manager.backup.ID;
 
 import java.io.Serializable;
 import java.util.Collection;
 
 /**
  * Struktura przechowująca zbiór plików posiadających dany tag.
- * 
- * @param <T>
- *            Typ tagu obsługiwanego przez instancję tej klasy
+ *
+ * @param <T> Typ tagu obsługiwanego przez instancję tej klasy
  * @author Maciej Poleski
  */
 class TagFiles<T extends Tag<T>> implements Serializable {
-	private Tag<T> tag;
-	private Collection<FileID> files;
+    private Tag<T> tag;
+    private Collection<ID> files;
 
-	public TagFiles() {
-	}
+    public TagFiles() {
+    }
 
-	/**
-	 * Zwraca tag który posiadają pliki zarządzane przez tą strukturę.
-	 * 
-	 * @return Tag który posiadają pliki zarządzane przez tą strukturę.
-	 */
-	public Tag<T> getTag() {
-		return null;
-	}
+    /**
+     * Zwraca tag który posiadają pliki zarządzane przez tą strukturę.
+     *
+     * @return Tag który posiadają pliki zarządzane przez tą strukturę.
+     */
+    public Tag<T> getTag() {
+        return null;
+    }
 
-	/**
-	 * Ustawia tag który posiadają pliki zarządzane przez tą strukturę.
-	 * 
-	 * @param tag
-	 *            Tag który posiadają pliki zarządzane przez tą strukturę
-	 */
-	public void setTag(Tag<T> tag) {
-	}
+    /**
+     * Ustawia tag który posiadają pliki zarządzane przez tą strukturę.
+     *
+     * @param tag Tag który posiadają pliki zarządzane przez tą strukturę
+     */
+    public void setTag(Tag<T> tag) {
+    }
 
-	/**
-	 * Dodaje plik do zbioru plików posiadających ten tag.
-	 * 
-	 * @param file
-	 *            Plik który zostanie oznaczony tym tagiem
-	 */
-	public void addFile(FileID file) {
-	}
+    /**
+     * Dodaje plik do zbioru plików posiadających ten tag.
+     *
+     * @param file Plik który zostanie oznaczony tym tagiem
+     */
+    public void addFile(ID file) {
+    }
 
-	/**
-	 * Usuwa plik ze zbioru plików posiadających ten tag.
-	 * 
-	 * @param file
-	 *            Plik który zostanie usunięty
-	 */
-	public void removeFile(FileID file) {
-	}
+    /**
+     * Usuwa plik ze zbioru plików posiadających ten tag.
+     *
+     * @param file Plik który zostanie usunięty
+     */
+    public void removeFile(ID file) {
+    }
 
-	/**
-	 * Zwraca kolekcję wszystkich plików posiadających ten tag
-	 * 
-	 * @return Kolekcja wszystkich plików posiadających ten tag
-	 */
-	public Collection<FileID> getFiles() {
-		return null;
-	}
+    /**
+     * Zwraca kolekcję wszystkich plików posiadających ten tag
+     *
+     * @return Kolekcja wszystkich plików posiadających ten tag
+     */
+    public Collection<ID> getFiles() {
+        return null;
+    }
 }
