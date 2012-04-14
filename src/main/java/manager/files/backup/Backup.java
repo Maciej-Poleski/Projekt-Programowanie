a5401df509bb707063a595ece64c4e53e7c0f804
Karol Banys <karol.banys@gmail.com>
2012-04-14 08:16:52 +0200

    Dodanie BackupOfFIles i zmiana backup.java

--- src/main/java/manager/files/backup/Backup.java
+++ src/main/java/manager/files/backup/Backup.java
@@ -1,11 +1,12 @@
 package manager.files.backup;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.Date;
-import java.util.List;
 
 import manager.files.FileID;
 import manager.files.OperationInterruptedException;
+import manager.tags.MasterTag;
 
 public interface Backup {
 
@@ -13,35 +14,24 @@ public interface Backup {
 	 * Tworzy nową lokalizację dla backupu
 	 * @param backupLocation
 	 */
-	void createNewBackup(File backupLocation);
+	void createNewBackup(MasterTag masaterTag, File backupLocation);
 
 	/**
 	 * aktualizuje kopie zapasowa z wszystkimi zmianami dokonanymi od czasu
 	 * ostatniej synchronizacji.
-	 * 
-	 * @ param sciezka do katalogu @ author
+	 * @param Pobiera sciezka do katalogu, Mastertag
 	 */
-	void synchronizeBackup(File backupLocation)
-			throws OperationInterruptedException;
+	void synchronizeBackup(File backupLocation) throws OperationInterruptedException;
 
 	/**
 	 * Pozwala odzyskać plik przypadkiem usunięty
-	 * 
-	 * @ param ID pliku @ author
+	 * @param Pobiera ID pliku
 	 */
-	void retrieveFile(FileID file) throws OperationInterruptedException;
+	File retrieveFile(FileID file) throws OperationInterruptedException;
 
 	/**
-	 * 
-	 * @ return @ param @ author
+	 * Zwraca date danego backupu tego pliku
+	 * @return Zwraca Data @param Pobiera Plik
 	 */
-	List<Change> getListOfChanges(File backupLocation)
-			throws OperationInterruptedException;
-
-	/**
-	 * 
-	 * @ return @ param @ author
-	 */
-	Date getLastBackupDate(File backupLocation)
-			throws OperationInterruptedException;
+	Date getLastBackupDate(File backupLocation) throws OperationInterruptedException;
 }
