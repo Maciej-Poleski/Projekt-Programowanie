c4c4d28a51db179e7f67eb87fd98fa9bab6fa257
Marcin Regdos <mregdos@gmail.com>
2012-04-16 09:09:59 +0200

    Dodanie do głównego okna wszystkich filtrów które posiadają stworzone gui.

--- src/main/java/manager/editor/EditWindow.java
+++ src/main/java/manager/editor/EditWindow.java
@@ -18,15 +18,29 @@ public class EditWindow extends JFrame implements ActionListener  {
 	private PixelData PDImage;
 	private LinkedList<PixelData> history;
 	private ImageViewer mainImageViewer;
-	private String [] filterNames;
 	private String [] filterNamesGUI;
 	private String [] filterCategoryNamesGUI;
-	private IFilter [] filters;
+	//private IFilter [] filters;
 	private int [] filterType;
 	private JMenuItem [] JMenuFilterButtons;
-	private JMenuItem [] JMenuFilterCategories;
-	private int nFilters=2;
-	private int nCategories=1;
+	private JMenu [] JMenuFilterCategories;
+	private FilterGUI [] filters;
+	private int nFilters=11;
+	private int nCategories=4;
+	private static class FilterGUI{
+		String name, nameGUI;
+		FWindowType window;
+		IFilter filter;
+		FilterGUI(String name, String nameGUI, FWindowType window, IFilter Filter){
+			this.name=name;
+			this.nameGUI=nameGUI;
+			this.window=window;
+			this.filter=Filter;
+		}
+	}
+	private enum FWindowType{
+		WindowRange		
+	}
 	private void InitGui(){
 		setTitle("Edytor plików graficznych");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,6 +64,30 @@ public class EditWindow extends JFrame implements ActionListener  {
 		bClose.addActionListener(this);
 
 	}
+	private void InitFiltersToGUI(){
+		filterNamesGUI =new String[]{
+				"Detekcja krawędzi", "Filtr LUT", "Filtr matrycowy", "Histogram", "Obrót w lewo", "Obrót w prawo", "Symetria", "Uwypuklenie", "Wygładzenie", "Wyostrzanie", "Zmiana rozmiaru"
+		};
+		filterCategoryNamesGUI=new String[]{
+				"Filtry podstawowe", "Filtry kolorystyczne","Inne", "Filtry kanałowe",  
+		};
+		filters=new FilterGUI[]{
+				new FilterGUI("FilterBrightness","Jasność", FWindowType.WindowRange, new FilterBrightness() ),
+				new FilterGUI("FilterContrast","Kontrast", FWindowType.WindowRange, new FilterContrast() ),
+				new FilterGUI("FilterGamma","Korekta gamma", FWindowType.WindowRange, new FilterGamma() ),
+				new FilterGUI("FilterExposure","Korekta ekspozycji", FWindowType.WindowRange, new FilterExposure() ),
+				new FilterGUI("FilterSepia","Sepia", FWindowType.WindowRange, new FilterSepia() ),
+				new FilterGUI("FilterColorAccent","Akcent kolorystyczny", FWindowType.WindowRange, new FilterColorAccent() ),
+				new FilterGUI("FilterBinaryzation","Binaryzacja", FWindowType.WindowRange, new FilterBinaryzation() ),
+				new FilterGUI("FilterSolarize","Solaryzacja", FWindowType.WindowRange, new FilterSolarize() ),
+				new FilterGUI("FilterRGBCorrection","Korekta RGB", FWindowType.WindowRange, new FilterRGBCorrection() ),
+				new FilterGUI("FilterCMYCorrection","Korekta CMY", FWindowType.WindowRange, new FilterCMYCorrection() ),
+				new FilterGUI("FilterHSBCorrection", "Korekta HSV", FWindowType.WindowRange, new FilterHSVCorrection() ),
+		};
+		JMenuFilterButtons=new JMenuItem[nFilters];
+		JMenuFilterCategories=new JMenu[nCategories];
+
+	}
 	private void InitMenu(){
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -66,39 +104,27 @@ public class EditWindow extends JFrame implements ActionListener  {
 			menuBar.add(JMenuFilterCategories[i]);
 		}
 		for (int i=0; i<nFilters; ++i){
-			JMenuFilterButtons[i]=new JMenuItem(filterNamesGUI[i]);
-			JMenuFilterButtons[i].setActionCommand(filterNames[i]);
+			JMenuFilterButtons[i]=new JMenuItem(filters[i].nameGUI);
+			JMenuFilterButtons[i].setActionCommand(filters[i].name);
 			JMenuFilterButtons[i].addActionListener(this);
 		}
 		JMenuFilterCategories[0].add(JMenuFilterButtons[0]);
 		JMenuFilterCategories[0].add(JMenuFilterButtons[1]);
+		JMenuFilterCategories[0].add(JMenuFilterButtons[2]);
+		JMenuFilterCategories[0].add(JMenuFilterButtons[3]);
+		JMenuFilterCategories[1].add(JMenuFilterButtons[4]);
+		JMenuFilterCategories[1].add(JMenuFilterButtons[5]);
+		JMenuFilterCategories[2].add(JMenuFilterButtons[6]);
+		JMenuFilterCategories[2].add(JMenuFilterButtons[7]);
+		JMenuFilterCategories[3].add(JMenuFilterButtons[8]);
+		JMenuFilterCategories[3].add(JMenuFilterButtons[9]);
+		JMenuFilterCategories[3].add(JMenuFilterButtons[10]);
 	}
 
 	/**
 	 * Konstruktor - wymagany jest obraz do edycji
 	 * @param image - referencja do objektu klasy BufferedImage przechowująca obraz do edycji
 	 */
-	private void InitFiltersToGUI(){
-		filterNames =new String[]{
-				"FilterBinaryzation", "FilterSepia"
-		};
-		filterNamesGUI =new String[]{
-				"Binaryzacja", "Sepia"
-		};
-		filterCategoryNamesGUI=new String[]{
-				"Filtry podstawowe"
-		};
-		filters=new IFilter[]{
-				new FilterBinaryzation(), new FilterSepia()		
-		};
-		//(1,2..)= (WindowRange, WindowResize, WindowMatrix, WindowLUT..)
-		filterType=new int[]{
-				1, 1		
-		};
-		JMenuFilterButtons=new JMenuItem[nFilters];
-		JMenuFilterCategories=new JMenu[nCategories];
-		
-	}
 	public EditWindow(BufferedImage image) {
 		InitFiltersToGUI();
 		mainImageViewer=new ImageViewer(image, 400, 400);
@@ -141,11 +167,11 @@ public class EditWindow extends JFrame implements ActionListener  {
 			this.dispose();
 			return;
 		} 
-		for (int i=0; i<filterNames.length; ++i){
-			if (e.getActionCommand().equals(filterNames[i])) {
-				switch(filterType[i]){
-				case 1: apply (new WindowRange(PDImage, (IFilterRange)filters[i], filterNamesGUI[i]).showDialog()); break;
-				case 2: apply (new WindowResize(PDImage,  filterNamesGUI[i]).showDialog()); break;
+		for (int i=0; i<filters.length; ++i){
+			if (e.getActionCommand().equals(filters[i].name)) {
+				switch(filters[i].window){
+				case WindowRange: apply (new WindowRange(PDImage, (IFilterRange)filters[i].filter, filters[i].nameGUI).showDialog()); break;
+				//case 2: apply (new WindowResize(PDImage,  filters[i].nameGUI).showDialog()); break;
 				}
 				return;
 			}
