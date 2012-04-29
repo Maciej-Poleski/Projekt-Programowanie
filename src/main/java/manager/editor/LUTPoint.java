8cd98212917cb51f5f2e99e640dbb796b1a28b23
Bińkowski <mikbinkowski@gmail.com>
2012-04-29 23:24:25 +0200

    Zmiany  sugerowane w uwagach 2.4, 2.7, 2.8

--- src/main/java/manager/editor/LUTPoint.java
+++ src/main/java/manager/editor/LUTPoint.java
@@ -8,9 +8,9 @@ import java.awt.*;
  */
 
 public class LUTPoint{
-    int x;
-    int y;
-    Rectangle rect;
+    private int x;
+    private int y;
+    private Rectangle rect;
 
     /**
      * Kontruktor wymaga podania współrzędnych
@@ -39,5 +39,41 @@ public class LUTPoint{
      */
     public void updateLocation(){
         this.rect.setLocation(x-5,y-5);
+    }
+    
+    /**
+     * Zmienia bądź ustanawia współrzędną X punktu. Po zmianie uaktualnia
+     * położenie pola odpowiadającego za detekcję kliknięcia w ten punkt
+     * @param x - nowa współrzędna X punktu
+     */
+    public void setX(int x){
+        this.x=x;
+        updateLocation();
+    }
+    
+    /**
+     * Zmienia bądź ustanawia współrzędną Y punktu. Po zmianie uaktualnia
+     * położenie pola odpowiadającego za detekcję kliknięcia w ten punkt
+     * @param y - nowa współrzędna Y punktu
+     */
+    public void setY(int y){
+        this.y=y;
+        updateLocation();
+    }
+
+    /**
+     * Zwraca wartość współrzędnej X punktu.
+     * @return - współrzędna X punktu
+     */
+    public int getX(){
+        return this.x;
+    }
+
+    /**
+     * Zwraca wartość współrzędnej Y punktu.
+     * @return - współrzędna Y punktu
+     */
+    public int getY(){
+        return this.y;
     }
 }
